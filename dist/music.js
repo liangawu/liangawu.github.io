fdd8a8d6a81dffe05b11976e7a99f01ebc7f1059
liangawu <mel1098478823@163.com>
2021-07-02 13:38:17 +0800

    Site updated: 2021-07-02 13:38:16

--- dist/music.js
+++ dist/music.js
@@ -8,29 +8,29 @@ const ap = new APlayer({
 	{
         name: "四季予你",
         artist: '程响',
-        url: 'http://mp.333ttt.com/mp3music/159747353.mp3',
-        cover: 'http://lc-7VDt0zMW.cn-n1.lcfile.com/920c60ec63f92fb9e8e5.png/%E7%A8%8B%E5%93%8D.png',
+        url: 'https://sharefs.ali.kugou.com/202107021323/a06a486d3163a8f12a7d7e687570dece/KGTX/CLTX001/d3ed05ada906113520be598e784c038e.mp3',
+        cover: 'https://i.loli.net/2021/07/02/VqcxYifK7ulCSpw.jpg',
 		theme: '#ebd0c2'
       },
 	  {
         name: "紅蓮華 (THE FIRST TAKE Studio Ver.)",
         artist: 'LiSA',
-        url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_156409300&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
-        cover: 'http://lc-7VDt0zMW.cn-n1.lcfile.com/3c4b860fbc9e78ec6d89.png/Lisa.png',
+        url: 'https://sharefs.ali.kugou.com/202107021324/fe88c804ec7df047c1e5373688b09865/G159/M0A/00/0D/f5QEAFy9LYSAFvRfADme8sxJysc695.mp3',
+        cover: 'https://i.loli.net/2021/07/02/8rUqTXK2l3SIbLc.jpg',
 		theme: '#46718b'
       },
 	   {
         name: "坠",
         artist: '添儿呗',
-        url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_143873609&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
-        cover: 'http://lc-7VDt0zMW.cn-n1.lcfile.com/e3d3e91094f763774e7f.png/%E5%9D%A0song.png',
+        url: 'https://sharefs.ali.kugou.com/202107021325/3226e26dbe0c962c60630a4f49bbeaa6/G226/M04/14/17/gocBAF75az-Ac0WtADT3wRabL7o158.mp3',
+        cover: 'https://i.loli.net/2021/07/02/pEucMC5t4lVUzTb.jpg',
 		theme: '#ebd0c2'
       },
       {
         name: '你瞒我瞒',
         artist: '陈柏宇',
-        url: 'http://mp4-333ttt.qqxzb-img.com/2015/1/04/10/97101122182.mp3',
-        cover: 'http://lc-7VDt0zMW.cn-n1.lcfile.com/9a0cb0875355571dc1da.png/%E9%99%88%E6%9F%8F%E5%AE%87.png',
+        url: 'https://sharefs.ali.kugou.com/202107021327/5cb8bd41baae1a9a20423208c0cb988e/KGTX/CLTX001/69b1328c3bd93c9405c92eb759299a0a.mp3',
+        cover: 'https://i.loli.net/2021/07/02/dwRFjhWLrCm8kHc.jpg',
 		theme: '#46718b'
       }
     ]
